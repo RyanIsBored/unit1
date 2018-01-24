5230fe0de20ae2799dc19cde5ef1ef718fed7c39
RyanIsBored <35492798+RyanIsBored@users.noreply.github.com>
2018-01-24 08:30:02 -0500

    Updated from Brython Server: 1/24/2018 8:30:01 AM

--- nameAge.py
+++ nameAge.py
@@ -3,4 +3,8 @@
 
 fullname= input( 'Your full name: ')
 firstname, lastname = fullname.split()
-print( 'Your first name has '(len(firstname)))
+age= int(input( 'Enter your age: '))
+print( 'Your first name has',(len(firstname)), 'letters.')
+print( 'Your last name has',(len(lastname)), 'letters.')
+nextyear= (age++1)
+print( 'Next year you will be',(nextyear), 'years old.')
